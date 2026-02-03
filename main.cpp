b7a25047771a20a9774618c8675470a654de803e
jaber <jaber@hyperloop.Jaber>
2026-02-03 10:38:24 +0600

    format

--- main.cpp
+++ main.cpp
@@ -1,48 +1,46 @@
 #include "raylib.h"
 
 int main() {
-  Color colors[] = {LIGHTGRAY, GRAY,    DARKGRAY,  YELLOW, GOLD,   ORANGE,
-                    PINK,      RED,     MAROON,    GREEN,  LIME,   DARKGREEN,
-                    SKYBLUE,   BLUE,    DARKBLUE,  PURPLE, VIOLET, DARKPURPLE,
-                    BEIGE,     BROWN,   DARKBROWN, WHITE,  BLACK,  BLANK,
-                    MAGENTA,   RAYWHITE};
+    Color colors[] = {LIGHTGRAY, GRAY,  DARKGRAY,  YELLOW,  GOLD,  ORANGE,   PINK,    RED,     MAROON,
+                      GREEN,     LIME,  DARKGREEN, SKYBLUE, BLUE,  DARKBLUE, PURPLE,  VIOLET,  DARKPURPLE,
+                      BEIGE,     BROWN, DARKBROWN, WHITE,   BLACK, BLANK,    MAGENTA, RAYWHITE};
 
-  int colorCount = sizeof(colors) / sizeof(colors[0]);
+    int colorCount = sizeof(colors) / sizeof(colors[0]);
 
-  const int screenWidth = 2048;
-  const int screenHeight = 1152;
+    const int screenWidth  = 2048;
+    const int screenHeight = 1152;
 
-  InitWindow(screenWidth, screenHeight, "My first raylib project");
-  SetTargetFPS(120);
+    InitWindow(screenWidth, screenHeight, "My first raylib project");
+    SetTargetFPS(120);
 
-  ChangeDirectory(GetApplicationDirectory());
+    ChangeDirectory(GetApplicationDirectory());
 
-  Font myfont = LoadFontEx("resources/zh-cn.ttf", 96, 0, 0);
-  SetTextureFilter(myfont.texture, TEXTURE_FILTER_BILINEAR);
+    Font myfont = LoadFontEx("resources/zh-cn.ttf", 96, 0, 0);
+    SetTextureFilter(myfont.texture, TEXTURE_FILTER_BILINEAR);
 
-  int distance = 40;
+    int distance = 40;
 
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
 
-    for (int x = 0; x < screenWidth; x += distance) {
-      for (int y = 0; y < screenHeight; y += distance) {
+        for (int x = 0; x < screenWidth; x += distance) {
+            for (int y = 0; y < screenHeight; y += distance) {
 
-        Vector2 pos = {(float)x, (float)y};
+                Vector2 pos = {(float) x, (float) y};
 
-        int colorIndex = (x / distance) % colorCount;
+                int colorIndex = (x / distance) % colorCount;
 
-        DrawTextEx(myfont, "0", pos, 40, 2, colors[colorIndex]);
-      }
+                DrawTextEx(myfont, "0", pos, 40, 2, colors[colorIndex]);
+            }
+        }
+
+        EndDrawing();
     }
 
-    EndDrawing();
-  }
+    // 4. Cleanup
+    UnloadFont(myfont);
+    CloseWindow();
 
-  // 4. Cleanup
-  UnloadFont(myfont);
-  CloseWindow();
-
-  return 0;
+    return 0;
 }
